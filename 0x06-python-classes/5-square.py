c465b5b40360d42855112ab885b48b01bae6e999
kiplrry <kiplarry19@gmail.com>
2022-12-21 23:39:20 +0300

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -38,18 +38,3 @@ class Square:
                 for j in range(self.__size):
                     print("#", end="")
                 print()
-
-my_square = Square(3)
-my_square.my_print()
-
-print("--")
-
-my_square.size = 10
-my_square.my_print()
-
-print("--")
-
-my_square.size = 0
-my_square.my_print()
-
-print("--")
