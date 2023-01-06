4934dd6ad7662c05113f284455859643b211f566
kiplrry <kiplarry19@gmail.com>
2023-01-06 18:20:01 +0300

    5-rectangle.py

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -74,13 +74,3 @@ class Rectangle:
     def __del__(self):
         """deletes an instance"""
         print("Bye rectangle...")
-
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-del my_rectangle
-
-try:
-    print(my_rectangle)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
