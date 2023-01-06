97c007b55dda82eaf8e2654cacb1c0d758fc78e7
kiplrry <kiplarry19@gmail.com>
2023-01-06 16:50:02 +0300

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Define a class Rectangle """
+
+
+class Rectangle:
+    """ Represents a Rectangle instance """
+    pass
+
+my_rectangle = Rectangle()
+print(type(my_rectangle))
+print(my_rectangle.__dict__)
