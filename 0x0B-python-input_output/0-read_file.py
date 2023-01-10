ecc77df7ab9ef279b68f755326eebb5be5be31ef
kiplrry <kiplarry19@gmail.com>
2023-01-10 22:50:40 +0300

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""readng a file"""
+
+
+def read_file(filename=""):
+    """func to read file
+    Args:
+        filename: the filename
+    """
+    with open(filename, "r", encoding="utf-8") as myFile:
+        myFile.read()
