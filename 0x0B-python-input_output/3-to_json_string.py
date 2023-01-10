82dfc1adcad6eeda514db96cef6a1d6a6d5b0daa
kiplrry <kiplarry19@gmail.com>
2023-01-10 23:25:49 +0300

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""return json"""
+import json
+
+def to_json_string(my_obj):
+    """function to return JSON from python
+    Args:
+        my_obj: object
+    """
+    return (json.dumps(my_obj))
