3257ec2b91666e09c7adb06d8c3f83c654d88233
kiplrry <kiplarry19@gmail.com>
2023-01-10 23:11:16 +0300

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Append to file"""
+
+
+def append_file(filename="", text=""):
+    """wite to a file
+    Args:
+        filename: name of the file
+        text: text to to written to the file
+    """
+    with open(filename, "a", encoding="utf-8") as myFile:
+        return(myFile.write(text))
