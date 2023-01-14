eb8e40586f56689bbde3ed6e0a8a93c9a21ff805
kiplrry <kiplarry19@gmail.com>
2023-01-14 12:57:55 +0300

    0-add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+imports the function def add(a, b): from the file
+add_0.py and prints the result of the addition 1 + 2 = 3
+"""
+if __name__ = "__main__":
+    from add_o.py import add
+
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a, b)}")
